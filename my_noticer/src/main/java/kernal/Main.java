2d2ec2320569ed67753c01d10f881d5a2671b320
610621215 <34718352+610621215@users.noreply.github.com>
2021-05-21 13:19:48 +0800

    Update Main.java

--- my_noticer/src/main/java/kernal/Main.java
+++ my_noticer/src/main/java/kernal/Main.java
@@ -4,11 +4,6 @@ import ui.*;
 public class Main {
     public static void main(String[]args){
         System.out.println("halo world");
-//        String host = "imap.gmail.com";
-//        String mailStoreType = "imap";
-//        String username = "codinglifehappy@gmail.com";
-//        String password = "crycry476674";
-//        MailReader.check(host, mailStoreType, username, password);
         new main_ui().dowork();
     }
 }
